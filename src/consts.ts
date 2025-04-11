f651cef21961765c0a0b586adf511ba274281d4f
Mahmoud Sakr <dev@sakr.me>
2025-04-11 22:28:32 +0200

    Update SITE_TITLE

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 import { execSync } from 'node:child_process';
 
-export const SITE_TITLE = 'Mahmoud\'s';
+export const SITE_TITLE = 'Mabmoud\'s';
 export const SITE_DESCRIPTION = 'Welcome to yet another blog website!';
 export const COMMIT_HASH = execSync('git rev-parse --short HEAD').toString().trim();
 
